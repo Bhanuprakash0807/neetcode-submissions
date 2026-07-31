0eacf75d563c990285553bdf9ec25c0ac4d2346f
Reddem Bhanu Prakash Reddy <reddembhanu368@gmail.com>
2026-07-31 09:26:08 +0530

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+       map<int,int> mpp;
+       int n=hand.size();
+       for(int i=0;i<n;i++){
+            mpp[hand[i]]++;
+       }
+       vector<int> v;
+       for(auto& it:mpp){
+        v.push_back(it.first);
+       }
+       sort(v.begin(),v.end(),greater<int>());
+       for(int i=0;i<v.size();i++){
+        if(mpp[v[i]]==0){
+            continue;
+        }
+        int num=v[i];
+        int fre=mpp[num];
+        int gs=groupSize-1;
+        num=num-1;
+        while(gs){
+            if(mpp[num]<fre){
+                return false;
+            }
+            mpp[num]-=fre;
+            num--;
+            gs--;
+        }
+       }
+       return true;
+    }
+};
